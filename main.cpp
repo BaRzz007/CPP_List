04bf864014b47f652b3a5254d1c8c2597dc69a0a
Ezekiel Bassey <bazzdude007@gmail.com>
2025-11-15 01:35:27 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
-#include "list_obj.h"
+#include <string>
+#include "todo_list.h"
 
 
 int main(int argc, char **argv)
 {
-  my_list todo_list = new my_list;
-
-  list_item item = new list_tem;
-  list_item item2 = new list_item;
   
-  todo_list.add_item(item);
-  todo_list.add_item(item2);
+  TodoList market_list = new TodoList;
 
-  todo_list.print_all();
+  std:string item = "Buy some tomato";
+  std:string item2 = "Buy fried plantain";
+  
+  todo_list.addItem(item);
+  todo_list.addItem(item2);
+
+  todo_list.printAll();
 
   todo_list.remove(1);
 
