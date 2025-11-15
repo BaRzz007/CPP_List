67a21947db061f49adc5fc75d1e31173bc5bd0c1
Ezekiel Bassey <bazzdude007@gmail.com>
2025-11-15 02:32:49 +0100

    Update todo_list.h

--- todo_list.h
+++ todo_list.h
@@ -9,16 +9,13 @@ private:
   std::string todo;
 
 public:
-  Todo_list(){
-    index = 0;
-  }
-  
   Todo_list *next;
 
+  Todo_list();
   void printAll();
   std::string getTodo(int index);
   void remove(int index);
-  void addItem(std::string todo);
+  void addItem(std::string &todo);
 };
 
 #endif //TODO_H
