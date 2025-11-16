d897ae2b5aa238594a4b1a3437126a16fd5dfe40
Ezekiel Bassey <bazzdude007@gmail.com>
2025-11-16 16:22:36 +0100

    Update todo_list.h

--- todo_list.h
+++ todo_list.h
@@ -14,10 +14,10 @@ class Todo_list {
     void printList();
     std::string getItem(int index);
     void remove(int index);
-    void remove(std::string &todoString);
-    void append(std::string &new_item);
-    void insert(std::string &new_item, int index);
-    void push(std::string &new_item);
+    void remove(std::string todoString);
+    void append(std::string new_item);
+    void insert(std::string new_item, int index);
+    void push(std::string new_item);
 };
 
 
