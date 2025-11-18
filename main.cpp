edec513ffb80cd4b6c66d13cc3a92ba99dfbddee
Ezekiel Bassey <bazzdude007@gmail.com>
2025-11-18 20:51:38 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -19,10 +19,11 @@ int main()
   market_list.printList();
 
   market_list.remove(1); //remove node by index
-  market_list.remove("Buy some tomato"); //remove node by value
+  //market_list.remove("Buy some tomato"); //remove node by value
   market_list.clearList(); //clear the linked list
 
   market_list.remove(0); //attempt to delete item from an empty list returns an error stating that the list is empty
   
   return (0);
 }
+
