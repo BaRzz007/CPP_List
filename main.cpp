a37a68b466e290ad52ab8412b5f96c92ced02efd
Ezekiel Bassey <bazzdude007@gmail.com>
2025-11-18 17:53:53 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,28 @@
 #include <iostream>
 #include <string>
-#include <stdio.h>
 #include "todo_list.hpp"
 
 
 int main()
 {
-  printf("Good old printf");
-  std::cout << "first line" << std::endl;
-  Todo_list market_list;
-  market_list.append("Buy some tomato");
-  market_list.append("Buy fried plantain");
-  market_list.append("Get a notepad for study");
+  Todo_list market_list; //creates new Linked-List
+  
+  market_list.append("Buy some tomato"); //adds an item to the end of the list
+  market_list.append("Buy fried plantain"); //adds an item to the end of the list
+  market_list.append("Get a notepad for study"); //adds an item to the end of the list
+  market_list.printList(); //see all the items in the list
 
+  market_list.push("Priority: Restock on toiletories"); //adds an item to the beginning of the list
   market_list.printList();
 
-  std::cout << "I got here" << std::endl;
+  market_list.insert("Get a brown bag", 3); //adds an item to the middle of the list at index 3
+  market_list.printList();
 
+  market_list.remove(1); //remove node by index
+  market_list.remove("Buy some tomato"); //remove node by value
+  market_list.clearList(); //clear the linked list
+
+  market_list.remove(0); //attempt to delete item from an empty list returns an error stating that the list is empty
+  
   return (0);
 }
